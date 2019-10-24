81ad36ccbfe2450f13b92984883984744dc8173f
guanyu yue <guanyuyue@guanyudeMBP.lan>
2019-10-25 02:10:48 +0800

    测试打包jar执行失败

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
-package main.java;
-
-import org.yaml.snakeyaml.Yaml;
-
 public class Main {
     public static void main(String[] args) {
-        Yaml yaml = new Yaml();
-        System.out.println(yaml);
         System.out.println("test Main Class");
     }
 }
