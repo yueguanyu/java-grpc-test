58056dc0fbb98152ab28a797f84f9bcf1a328181
guanyu <yue.guanyu@hotmail.com>
2019-10-25 20:52:41 +0800

    prepare for grpc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import org.yaml.snakeyaml.Yaml;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //初始化Yaml解析器
+        Yaml yaml = new Yaml();
+        File f=new File("src/main/config/test.yaml");
+        //读入文件
+        Map result= (Map) yaml.load(new FileInputStream(f));
+        System.out.println(result);
+        System.out.println((int)result.get("port"));
+        System.out.println(result.get("children"));
         System.out.println("test Main Class");
     }
 }
--- src/maven/selenium/testng/ImJa.java
+++
@@ -1,7 +0,0 @@
-package maven.selenium.testng;
-
-public class ImJa {
-    public void imJ() {
-        System.out.println("I am imJ!");
-    }
-}
